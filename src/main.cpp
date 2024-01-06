df7d2c6eedf40e6b47afbedc12ca4e661a3ae216
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-06 02:16:12 +0100

    Added message

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <util/colt_config.h>
+#include <io/print.h>
+
+using namespace clt;
 
 int main()
 {
-  
+  io::print_message("Hello COLT!\nv{} on {} ({})",
+    COLT_VERSION_STRING, COLT_OS_STRING, COLT_CONFIG_STRING);
 }
