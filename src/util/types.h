e2469cbb6b82a122b51a9dfacd565187350aa05f
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-06 05:45:28 +0100

    Added Error

--- src/util/types.h
+++ src/util/types.h
@@ -1,3 +1,23 @@
+/*****************************************************************//**
+ * @file   types.h
+ * @brief  Contains useful typedefs.
+ * The `out` type can be used to mark parameters as being uninitialized.
+ * These parameter must then be initialized before the function returns.
+ * @code{.cpp}
+ * void foo(out<int> a)
+ * {
+ *   //...
+ *   a.init(10);
+ * }
+ * @endcode
+ * 
+ * The `Error` type is a simple boolean flag that represents either a
+ * success or a failure state. An `Error` must be checked before its
+ * destructor runs.
+ * 
+ * @author RPC
+ * @date   January 2024
+ *********************************************************************/
 #ifndef HG_COLT_TYPES
 #define HG_COLT_TYPES
 
@@ -105,12 +125,122 @@ namespace clt
       /// @return Reference to the object
       constexpr T& get() const noexcept { return obj; }
     };
+
+    /// @brief Represents an Error (with runtime checks)
+    class ErrorDebug
+    {
+      /// @brief The source location
+      std::source_location src;
+      /// @brief True if represents an error
+      bool iserror;
+      /// @brief True if the error state was read at least once
+      mutable bool is_checked = false;
+
+      /// @brief Constructs an error with a specific state.
+      /// The constructor is marked private so that the more readable sucess and error
+      /// methods are used.
+      /// @param is_error True if an error
+      ErrorDebug(bool is_error, const std::source_location& src)
+        : src(src), iserror(is_error) {}
+
+      /// @brief Asserts that the state was read at least once
+      void assert_checked() noexcept
+      {
+        if (!is_checked)
+          clt::unreachable("'Error' result must be checked!", src);
+      }
+
+    public:
+      ErrorDebug(const ErrorDebug&) = delete;
+      ErrorDebug operator=(const ErrorDebug&) = delete;
+
+      // Move Constructor (steals the state of 'move')
+      constexpr ErrorDebug(ErrorDebug&& move) noexcept
+        : iserror(move.iserror), is_checked(std::exchange(move.is_checked, true)) {}
+      // Move assignment operator
+      constexpr ErrorDebug& operator=(ErrorDebug&& move) noexcept
+      {
+        assert_checked();
+        iserror = move.iserror;
+        is_checked = std::exchange(move.is_checked, true);
+      }
+
+      /// @brief Constructs a success
+      /// @return State representing a success
+      static auto success(std::source_location src = std::source_location::current()) noexcept { return ErrorDebug(false, src); }
+      /// @brief Constructs an error
+      /// @return State representing an error
+      static auto error(std::source_location src = std::source_location::current()) noexcept { return ErrorDebug(true, src); }
+
+      /// @brief Check if the state represents an error
+      constexpr bool is_error() const noexcept
+      {
+        is_checked = true;
+        return iserror;
+      }
+      
+      /// @brief Check if the state represents a success
+      constexpr bool is_success() const noexcept
+      {
+        return !is_error();
+      }
+
+      /// @brief Check if the state represents a success
+      explicit constexpr operator bool() const noexcept { return is_success(); }
+
+      /// @brief Ensures the state was at least read once
+      ~ErrorDebug()
+      {
+        assert_checked();
+      }
+    };
+
+    /// @brief Represents an Error (without any runtime checks)
+    class ErrorRelease
+    {
+      /// @brief True if error
+      bool iserror;
+
+      /// @brief Constructs an error with a specific state.
+      /// The constructor is marked private so that the more readable sucess and error
+      /// methods are used.
+      /// @param is_error True if an error
+      constexpr ErrorRelease(bool is_error)
+        : iserror(is_error) {}
+
+    public:
+      ErrorRelease(const ErrorRelease&) = delete;
+      ErrorRelease& operator=(const ErrorRelease&) = delete;
+      
+      // Default move constructor
+      constexpr ErrorRelease(ErrorRelease&&) noexcept = default;
+      // Default move assignment operator
+      constexpr ErrorRelease& operator=(ErrorRelease&&) noexcept = default;
+
+      /// @brief Constructs a success
+      /// @return State representing a success
+      constexpr static auto success() noexcept { return ErrorRelease(false); }
+      /// @brief Constructs an error
+      /// @return State representing an error
+      constexpr static auto error() noexcept { return ErrorRelease(true); }
+
+      /// @brief Check if the state represents an error
+      constexpr bool is_error() const noexcept { return iserror; }
+      /// @brief Check if the state represents a success
+      constexpr bool is_success() const noexcept { return !iserror; }
+
+      /// @brief Check if the state represents a success
+      explicit constexpr operator bool() const noexcept { return is_success(); }
+    };
   }
 
   template<typename T> requires (!std::is_reference_v<T>) && (!std::is_const_v<T>)
   /// @brief An 'out' parameter, which is a uninitialized reference that must be
   /// initialized inside the function. 'out' parameters are taken as is (without const or references).
   using out = std::add_const_t<std::conditional_t<isDebugBuild(), details::OutDebug<T>, details::OutRelease<T>>>&;
+
+  /// @brief Boolean that represents a success/failure state that must be checked.
+  using Error = std::conditional_t<isDebugBuild(), details::ErrorDebug, details::ErrorRelease>;
 }
 
 #endif // !HG_COLT_TYPES
