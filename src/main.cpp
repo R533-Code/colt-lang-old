3f5f2e34bf1312dd7efe7ded3c3b4f089d5a4521
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-06 22:21:33 +0100

    Added callback to handle null

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ using namespace clt;
 int main(int argc, char** argv)
 {
   cl::parse_command_line_options<CMDs>(argc, argv);
+  mem::global_on_null([]() noexcept { io::print_fatal("Could not allocate memory!"); });
 
   io::print_message("Hello COLT!\nv{} on {} ({})",
     COLT_VERSION_STRING, COLT_OS_STRING, COLT_CONFIG_STRING);
