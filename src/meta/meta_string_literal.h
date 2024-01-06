b5709f282af007b4050fec1f0fca37aae296428f
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-06 02:12:49 +0100

    Added a string literal

---
+++ src/meta/meta_string_literal.h
@@ -0,0 +1,60 @@
+/** @file string_literal.h
+* Contains a helper to pass string literals as template parameters.
+*/
+
+#ifndef HG_META_STRING_LITERAL
+#define HG_META_STRING_LITERAL
+
+#include <array>
+
+namespace clt::meta
+{
+  template <size_t N>
+  /// @brief Type to pass string literals as template parameters
+  struct StringLiteral
+  {
+    /// @brief Constructs a StringLiteral from a literal string
+    /// @param str The literal string
+    constexpr StringLiteral(const char(&str)[N])
+    {
+      std::copy_n(str, N, value);
+    }
+
+    /// @brief Size of the literal string (without NUL terminator)
+    /// @return Size of the literal string
+    static constexpr size_t size() noexcept { return N - 1; }
+
+    /// @brief The value of the literal string
+    char value[N];
+  };
+
+  template <StringLiteral... Strs>
+  /// @brief Concatenates StringView at compile time
+  struct join
+  {
+    /// @brief Concatenate all the StringView and returns an array storing the result
+    static constexpr auto impl() noexcept
+    {
+      constexpr std::size_t len = (Strs.size() + ... + 0);
+      std::array<char, len + 1> arr{};
+      auto append = [i = 0, &arr](auto const& s) mutable {
+        for (size_t j = 0; j < s.size(); j++) arr[i++] = s.value[j];
+        };
+      (append(Strs), ...);
+      arr[len] = '\0';
+      return arr;
+    }
+
+    /// @brief Array of characters representing concatenated string
+    static constexpr auto arr = impl();
+    
+    /// @brief Concatenation result
+    static constexpr const char* value{ arr.data() };
+  };
+  
+  template <StringLiteral... Strs>
+  /// @brief Short-hand for join<...>::value
+  static constexpr auto join_v = join<Strs...>::value;
+}
+
+#endif //!HG_META_STRING_LITERAL
