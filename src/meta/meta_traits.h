96f989f45f0f9f96cc66d72b7fa85cd138c39d85
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-06 21:46:33 +0100

    Added more traits

--- src/meta/meta_traits.h
+++ src/meta/meta_traits.h
@@ -9,9 +9,13 @@
 #define HG_META_TRAITS
 
 #include <type_traits>
+#include "util/config_type.h"
 
 namespace clt::meta
 {
+  /// @brief Empty struct helper
+  struct Empty {};
+
   template<typename Of, typename For>
   /// @brief Makes a type match the const-ness of another
   /// @tparam Of The type whose const-ness to match against
@@ -56,6 +60,55 @@ namespace clt::meta
   /// @tparam Of The type whose qualifiers to match against
   /// @tparam For The type to transform if necessary
   using match_cv_t = typename match_cv<Of, For>::type;
+
+  namespace details
+  {
+    template<typename T> requires (std::is_void_v<T>)
+      /// @brief Helper for sizeof_or_zero
+      /// @tparam T The type to check for
+      /// @return 0
+      consteval size_t sizeof_or_zero() noexcept
+    {
+      return 0;
+    }
+
+    template<typename T> requires (!std::is_void_v<T>)
+      /// @brief Helper for sizeof_or_zero
+      /// @tparam T The type to check for
+      /// @return sizeof(T)
+      consteval size_t sizeof_or_zero() noexcept
+    {
+      return sizeof(T);
+    }
+  }
+
+  template<typename T>
+  /// @brief Returns the sizeof a type or 0 if void
+  /// @tparam T The type
+  struct sizeof_or_zero
+  {
+    static constexpr size_t value = details::sizeof_or_zero<T>();
+  };
+
+  template<typename T>
+  /// @brief Shorthand for sizeof_or_zero<T>::value
+  /// @tparam T The type to check for
+  inline constexpr size_t sizeof_or_zero_v = sizeof_or_zero<T>::value;
+
+  template<typename Debug, typename Release>
+  /// @brief Chooses a type if on Debug configuration, or another for Release
+  /// @tparam Debug The type on Debug configuration
+  /// @tparam Release The type on Release configuration
+  struct for_debug_for_release
+  {
+    using type = std::conditional_t<clt::isDebugBuild(), Debug, Release>;
+  };
+
+  template<typename Debug, typename Release>
+  /// @brief Shorthand for 'for_debug_for_release<Debug, Release>::type'
+  /// @tparam Debug The type on Debug configuration
+  /// @tparam Release The type on Release configuration
+  using for_debug_for_release_t = typename for_debug_for_release<Debug, Release>::type;
 }
 
 #endif // !HG_META_TRAITS
