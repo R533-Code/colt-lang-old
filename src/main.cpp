4a1decb14061be352195552f7d80c60a14b27cd3
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-06 07:41:59 +0100

    Added command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <util/colt_pch.h>
-#include "util/colt_config.h"
+#include "args.h"
 
 using namespace clt;
 
-int main()
+int main(int argc, char** argv)
 {
+  cl::parse_command_line_options<CMDs>(argc, argv);
+
   io::print_message("Hello COLT!\nv{} on {} ({})",
     COLT_VERSION_STRING, COLT_OS_STRING, COLT_CONFIG_STRING);
+  
+  if (WaitForUserInput)
+    (void)getchar();
 }
