fd4b8facea8f2b24c87ca2883fca9c59bedcdc28
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-07 17:12:25 +0100

    Added test for local unique pointers

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <util/colt_pch.h>
 #include "args.h"
-#include "mem/global_alloc.h"
+#include "structs/unique_ptr.h"
 
 using namespace clt;
 
@@ -8,6 +8,11 @@ int main(int argc, char** argv)
 {
   mem::global_on_null([]() noexcept { io::print_fatal("Could not allocate memory!"); });
   cl::parse_command_line_options<CMDs>(argc, argv);
+
+  mem::StackAllocator<1024_B> stack;
+
+  auto a = make_local_unique<u32>(stack, 100);
+  io::print_message("{}: {}", a.get(), *a);
 
   io::print_message("Hello COLT!\nv{} on {} ({})",
     COLT_VERSION_STRING, COLT_OS_STRING, COLT_CONFIG_STRING);
