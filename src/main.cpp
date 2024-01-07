9e0ac5017b71698058158c98426a83892470d087
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-07 15:52:21 +0100

    Moved global_on_null

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ using namespace clt;
 
 int main(int argc, char** argv)
 {
-  cl::parse_command_line_options<CMDs>(argc, argv);
   mem::global_on_null([]() noexcept { io::print_fatal("Could not allocate memory!"); });
+  cl::parse_command_line_options<CMDs>(argc, argv);
 
   io::print_message("Hello COLT!\nv{} on {} ({})",
     COLT_VERSION_STRING, COLT_OS_STRING, COLT_CONFIG_STRING);
