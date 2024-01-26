52a8e15c649ae765ba2b45e1db04dd19c781caa8
R533-Code <raphaeldibnehmepc@gmail.com>
2024-01-26 19:08:22 +0100

    Tested Vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <util/colt_pch.h>
 #include "args.h"
 #include "structs/unique_ptr.h"
+#include "structs/vector.h"
 
 using namespace clt;
 
@@ -8,11 +9,6 @@ int main(int argc, const char** argv)
 {
   mem::global_on_null([]() noexcept { io::print_fatal("Could not allocate memory!"); });
   cl::parse_command_line_options<CMDs>(argc, argv);
-
-  mem::StackAllocator<1024_B> stack;
-
-  auto a = make_local_unique<u32>(stack, 100);
-  io::print_message("{}: {}", a.get(), *a);
 
   io::print_message("Hello COLT!\nv{} on {} ({})",
     COLT_VERSION_STRING, COLT_OS_STRING, COLT_CONFIG_STRING);
