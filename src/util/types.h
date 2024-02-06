ed80f98ebc7ca47e8e53d7f02a498bce9635ab90
R533-Code <raphaeldibnehmepc@gmail.com>
2024-02-06 09:10:34 +0100

    Added bit types

--- src/util/types.h
+++ src/util/types.h
@@ -307,6 +307,209 @@ namespace clt
       }
     };
   }
+
+  namespace details
+  {
+    template<typename T> requires std::unsigned_integral<T> && (!std::same_as<T, bool>)
+      /// @brief Class representing common byte sizes, simplifying bitwise operations
+      class BitSet
+    {
+      /// @brief Underlying integer storing the value
+      T value = 0;
+
+    public:
+      /// @brief Constructs a BitSet with all the bits cleared (0)
+      constexpr BitSet() noexcept
+        : value(0) {}
+      /// @brief Constructs a BitSet from an Integer
+      /// @param value The value of the bit-set
+      constexpr BitSet(T value) noexcept
+        : value(value) {}
+      constexpr BitSet(const BitSet&)             noexcept = default;
+      constexpr BitSet(BitSet&&)                  noexcept = default;
+      constexpr BitSet& operator=(const BitSet&)  noexcept = default;
+      constexpr BitSet& operator=(BitSet&&)       noexcept = default;
+
+      /// @brief Returns the state of the nth-bit
+      /// @param n The index of the bit (starting at 0)
+      /// @return True if on (1)
+      constexpr bool operator[](size_t n)  const noexcept { return (value >> n) & 1U; }
+      /// @brief Check if the nth-bit is set (1)
+      /// @param n The index of the bit (starting at 0)
+      /// @return True if on (1)
+      constexpr bool is_set(size_t n)     const noexcept { return (value >> n) & 1U; }
+      /// @brief Check if the nth-bit is clear (0)
+      /// @param n The index of the bit (starting at 0)
+      /// @return True if off (0)
+      constexpr bool is_clr(size_t n)     const noexcept { return !is_set(n); }
+      /// @brief Check if all the bits are set
+      /// @return True if all the bits are on (1)
+      constexpr bool is_all_set()         const noexcept { return value == (T)(-1); }
+      /// @brief Check if at least a bit is set
+      /// @return True if at least a bit is on (1)
+      constexpr bool is_any_set()         const noexcept { return std::popcount(value) != 0; }
+      /// @brief Check if no bit is set
+      /// @return True if no bit is on (1)
+      constexpr bool is_none_set()        const noexcept { return value == 0; }
+
+      /// @brief Sets the state of the nth-bit to 1
+      /// @param n The index of the bit (starting at 0)
+      constexpr void set_n(size_t n) noexcept
+      {
+        assert_true("Invalid index!", n < sizeof(T) * 8);
+        value |= 1UL << n;
+      }
+
+      /// @brief Clears the state of the nth-bit (sets the bit to 0)
+      /// @param n The index of the bit (starting at 0)
+      constexpr void clr_n(size_t n) noexcept
+      {
+        assert_true("Invalid index!", n < sizeof(T) * 8);
+        value &= ~(1UL << n);
+      }
+
+      /// @brief Toggles the state of the nth-bit (if 0 -> 1, if 1 -> 0)
+      /// @param n The index of the bit (starting at 0)
+      constexpr void tgl_n(size_t n) noexcept
+      {
+        assert_true("Invalid index!", n < sizeof(T) * 8);
+        value ^= 1UL << n;
+      }
+
+      /// @brief Changes the state of the nth-bit to 'to'
+      /// @param n The index of the bit (starting at 0)
+      /// @param to The value to set the bit to
+      constexpr void chg_n(size_t n, bool to) noexcept
+      {
+        assert_true("Invalid index!", n < sizeof(T) * 8);
+        value ^= (-(T)to ^ value) & (1UL << n);
+      }
+
+      /// @brief Bitwise OR operator
+      /// @param byte The bit-set with which to perform the operation
+      /// @return New bit-set containing the result of the operation
+      constexpr BitSet operator|(BitSet byte) const noexcept { return value | byte.value; }
+      /// @brief Bitwise XOR operator
+      /// @param byte The bit-set with which to perform the operation
+      /// @return New bit-set containing the result of the operation
+      constexpr BitSet operator^(BitSet byte) const noexcept { return value ^ byte.value; }
+      /// @brief Bitwise AND operator
+      /// @param byte The bit-set with which to perform the operation
+      /// @return New bit-set containing the result of the operation
+      constexpr BitSet operator&(BitSet byte) const noexcept { return value & byte.value; }
+      /// @brief Shift Left operator
+      /// @param by By how many bits to shift
+      /// @return New bit-set containing the result of the operation
+      constexpr BitSet operator<<(size_t by) const noexcept
+      {
+        assert_true("Invalid bit-shift!", by < sizeof(T) * 8);
+        return value << by;
+      }
+
+      /// @brief Shift Right operator
+      /// @param by By how many bits to shift
+      /// @return New bit-set containing the result of the operation
+      constexpr BitSet operator>>(size_t by) const noexcept
+      {
+        assert_true("Invalid bit-shift!", by < sizeof(T) * 8);
+        return value >> by;
+      }
+
+      /// @brief Bitwise NOT operator
+      /// @return New bit-set containing the result of the operation
+      constexpr BitSet operator~() const noexcept { return ~value; }
+
+      /// @brief Bitwise OR operator
+      /// @param byte The bit-set with which to perform the operation
+      /// @return Self
+      constexpr BitSet& operator|=(BitSet byte) noexcept { value |= byte.value; return *this; }
+      /// @brief Bitwise XOR operator
+      /// @param byte The bit-set with which to perform the operation
+      /// @return Self
+      constexpr BitSet& operator^=(BitSet byte) noexcept { value ^= byte.value; return *this; }
+      /// @brief Bitwise AND operator
+      /// @param byte The bit-set with which to perform the operation
+      /// @return Self
+      constexpr BitSet& operator&=(BitSet byte) noexcept { value &= byte.value; return *this; }
+      /// @brief Shift Left operator
+      /// @param by By how many bits to shift
+      /// @return Self
+      constexpr BitSet& operator<<=(size_t by) noexcept
+      {
+        assert_true("Invalid bit-shift!", by < sizeof(T) * 8);
+        value <<= by; return *this;
+      }
+
+      /// @brief Shift Right operator
+      /// @param by By how many bits to shift
+      /// @return Self
+      constexpr BitSet& operator>>=(size_t by) noexcept
+      {
+        assert_true("Invalid bit-shift!", by < sizeof(T) * 8);
+        value >>= by; return *this;
+      }
+
+      /// @brief Counts the number of set bits
+      /// @return The number of bits that are set (=1)
+      constexpr size_t count() const noexcept { return std::popcount(value); }
+
+      /// @brief Returns the underlying integer storing the bit-set
+      /// @return Reference to the underlying integer
+      constexpr T& to_underlying() noexcept { return value; }
+      /// @brief Returns the underlying integer storing the bit-set
+      /// @return Reference to the underlying integer
+      constexpr const T& to_underlying() const noexcept { return value; }
+
+      /// @brief Sets all the bits to 0
+      /// @return Self
+      constexpr BitSet& clear() noexcept { value = 0; return *this; }
+
+      template<typename From>
+      constexpr BitSet bit_assign(From frm) noexcept
+      {
+        std::memcpy(&value, &frm, sizeof(From));
+        return *this;
+      }
+
+      template<typename To>
+      constexpr To as() const noexcept
+      {
+        To ret;
+        std::memcpy(&ret, &value, sizeof(To));
+        return ret;
+      }
+
+      /// @brief Underlying integer type storing the value
+      using underlying_type = T;
+    };
+  }
+
+  /// @brief 8-bit BitSet
+  using BYTE_t = details::BitSet<u8>;
+  /// @brief 16-bit BitSet
+  using WORD_t = details::BitSet<u16>;
+  /// @brief 32-bit BitSet
+  using DWORD_t = details::BitSet<u32>;
+  /// @brief 64-bit BitSet
+  using QWORD_t = details::BitSet<u64>;
+
+  template<typename T>
+  concept BitType = std::same_as<T, BYTE_t>
+    || std::same_as<T, WORD_t>
+    || std::same_as<T, DWORD_t>
+    || std::same_as<T, QWORD_t>;
+
+  template<BitType T, typename Wt> requires (sizeof(Wt) == sizeof(T))
+  /// @brief Converts a type to its bit-set equivalent
+  /// @tparam Wt The type to convert
+  /// @tparam T One of [BYTE, WORD, DWORD, QWORD]
+  /// @param what The value to convert
+  /// @return The converted value
+  constexpr T bit_as(const Wt& what) noexcept
+  {
+    using cnv_t = typename T::underlying_type;
+    return T(std::bit_cast<cnv_t>(what));
+  }
 }
 
 namespace clt::details
