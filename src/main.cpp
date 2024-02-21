70097a53e89e5aeabcedee051d33bfbf273d8328
R533-Code <raphaeldibnehmepc@gmail.com>
2024-02-21 18:31:41 +0100

    Removed testing function and added new include

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,17 @@
  *********************************************************************/
 #include <util/colt_pch.h>
 #include "args.h"
-#include "lex/colt_lexer.h"
-#include "err/composable_reporter.h"
+#include "util/run_tests.h"
 
 using namespace clt;
-
-void test()
-{
-  using namespace lng;
-
-  auto reporter = lng::make_error_reporter<lng::ConsoleReporter>();
-  while (true)
-  {
-    io::print<"">("Enter line to lex: ");
-    auto a = String::getLine(64, false);
-    if (a.is_error())
-      return;
-    const auto& str = *a;
-  
-    TokenBuffer buffer = lng::Lex(*reporter, str);
-    for (auto tkn : buffer.getTokens())
-      io::print("{}: line {} [{}:{}]",
-        tkn.getLexeme(), buffer.getLine(tkn),
-        buffer.getColumn(tkn), buffer.getSize(tkn));
-  }
-}
 
 int main(int argc, const char** argv)
 {
   mem::global_on_null([]() noexcept { io::print_fatal("Could not allocate memory!"); });
   cl::parse_command_line_options<CMDs>(argc, argv);
   
-  test();
+  // On debug configuration, runs tests
+  clt::run_tests();
 
   if (InputFile.empty())
     io::print_warn("REPL is not implemented...");
