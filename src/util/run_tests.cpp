067010c359ab603843b0a6355f60ecc2c26442e3
R533-Code <raphaeldibnehmepc@gmail.com>
2024-02-21 18:44:28 +0100

    Added body of run_tests()

--- src/util/run_tests.cpp
+++ src/util/run_tests.cpp
@@ -1,12 +1,32 @@
 #include "run_tests.h"
 #include "args.h"
+#include "lex/test_lexer.h"
 
 namespace clt
 {
 #ifdef COLT_DEBUG
   void run_tests() noexcept
   {
+    if (!RunTests)
+      return;
 
+    u32 error_count = 0;
+    u32 run_test_count = 0;
+    
+    if (!LexerTestFile.empty())
+    {
+      ++run_test_count;
+      test::test_lexer(LexerTestFile, error_count);
+    }
+
+    if (run_test_count == 0)
+    {
+      io::print_warn("{}-run-tests{} was specified but no tests were run!", io::BrightCyanF, io::Reset);
+      io::print_message("As an example, use {}-test-lexer{}={}<FILEPATH>{} to test the lexer.", io::BrightCyanF, io::Reset, io::BrightMagentaF, io::Reset);
+      return;
+    }
+    io::print_message("Tested {} features with {} error{}", run_test_count, error_count,
+      error_count ? "s." : ".");
   }
 #else
   void run_tests() noexcept
