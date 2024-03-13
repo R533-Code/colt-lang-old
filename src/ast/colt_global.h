6ee3267aa2dbd570c3d5a406733ee167c6c5dba7
R533-Code <raphaeldibnehmepc@gmail.com>
2024-03-13 11:39:05 +0100

    Added skeleton of GlobalVariant

--- src/ast/colt_global.h
+++ src/ast/colt_global.h
@@ -1,12 +1,248 @@
 #ifndef HG_COLT_GLOBAL
 #define HG_COLT_GLOBAL
 
+DECLARE_ENUM_WITH_TYPE(u8, clt::lng, GlobalID,
+  GLOBAL_FN,
+  GLOBAL_VAR,
+  GLOBAL_TYPE,
+  GLOBAL_ALIAS
+);
+
+/// @brief Macro Type List (with same index as GlobalID declaration!)
+#define COLTC_TYPE_LIST FnGlobal, VarGlobal, TypeGlobal, AliasGlobal
+
 namespace clt::lng
 {
+  /// @brief Base class of all types
+  class GlobalBase
+  {
+    static constexpr u64 BITS_FOR_ID = 7;
+
+    static_assert(clt::reflect<GlobalID>::max() < (u64)clt::pow((u64)2, BITS_FOR_ID),
+      "Not enough bits to represent GlobalID!");
+
+    /// @brief The ID of the type (used for down-casts)
+    u8 global_id : BITS_FOR_ID;
+    /// @brief True if private, false if public
+    u8 isprivate : 1;
+
+  public:
+    // No default constructor
+    constexpr GlobalBase() noexcept = delete;
+    /// @brief Constructs a GlobalBase
+    /// @param id The Global ID
+    constexpr GlobalBase(GlobalID id, bool is_private) noexcept
+      : global_id(static_cast<u8>(id)), isprivate(is_private) {}
+    // Move constructible and move assignable
+    constexpr GlobalBase(GlobalBase&&) noexcept = default;
+    constexpr GlobalBase(const GlobalBase&) noexcept = default;
+    constexpr GlobalBase& operator=(GlobalBase&&) noexcept = default;
+    constexpr GlobalBase& operator=(const GlobalBase&) noexcept = default;
+
+    /// @brief Check if the current global is private
+    /// @return True if private
+    constexpr bool isPrivate() const noexcept { return isprivate; }
+    /// @brief Check if the current global is public
+    /// @return True if public
+    constexpr bool isPublic() const noexcept { return !isprivate; }
+
+    /// @brief Returns the ID of the type (used for down-casts)
+    /// @return The GlobalID of the current type
+    constexpr GlobalID classof() const noexcept { return static_cast<GlobalID>(global_id); }
+
+    /// @brief Check if the Global is a function
+    /// @return True if function
+    constexpr bool isFn() const noexcept { return classof() == GlobalID::GLOBAL_FN; }
+    /// @brief Check if the Global is a global variable
+    /// @return True if variable
+    constexpr bool isVar() const noexcept { return classof() == GlobalID::GLOBAL_VAR; }
+    /// @brief Check if the Global is a type
+    /// @return True if type
+    constexpr bool isType() const noexcept { return classof() == GlobalID::GLOBAL_TYPE; }
+    /// @brief Check if the Global is an alias
+    /// @return True if an alias
+    constexpr bool isAlias() const noexcept { return classof() == GlobalID::GLOBAL_ALIAS; }
+  };
+
+  template<typename T>
+  /// @brief A ColtType provides its ID and is equality comparable
+  concept ColtGlobal = std::equality_comparable<T> && std::convertible_to<T, GlobalBase>&& requires (T a)
+  {
+    { a.classof() } -> std::same_as<GlobalID>;
+  };
+
+  // Forward declarations
+  class FnGlobal;
+  class VarGlobal;
+  class TypeGlobal;
+  class AliasGlobal;
+  class GlobalVariant;
+
+  template<typename T>
+  /// @brief Converts a type to its ID.
+  /// @return The ID representing 'T'
+  constexpr GlobalID TypeToGlobalID() noexcept
+  {
+    using enum GlobalID;
+
+    if constexpr (std::same_as<T, FnGlobal>)
+      return GLOBAL_FN;
+    if constexpr (std::same_as<T, VarGlobal>)
+      return GLOBAL_VAR;
+    if constexpr (std::same_as<T, TypeGlobal>)
+      return GLOBAL_TYPE;
+    if constexpr (std::same_as<T, AliasGlobal>)
+      return GLOBAL_ALIAS;
+  }
+
+  class FnGlobal
+    final : public GlobalBase {};
+
+  class VarGlobal
+    final : public GlobalBase {};
+
+  class TypeGlobal
+    final : public GlobalBase {};
+
+  /// @brief Represents an alias to another global
+  class AliasGlobal
+    final : public GlobalBase
+  {
+    /// @brief The aliased global
+    GlobalVariant& alias_to;
+
+  public:
+    /// @brief Constructs an alias to another global
+    /// @param alias_to The aliased global
+    /// @param is_private True if private
+    constexpr AliasGlobal(GlobalVariant& alias_to, bool is_private) noexcept
+      : GlobalBase(TypeToGlobalID<AliasGlobal>(), is_private), alias_to(alias_to) {}
+
+    /// @brief Returns the aliased global
+    /// @return The aliased global
+    constexpr GlobalVariant& getAliasTo() const noexcept { return alias_to; }
+  };
+
+  /// @brief Macro used to generate getUnionMember body
+#define COLTC_getUnionMember \
+  if constexpr (std::same_as<T, FnGlobal>) \
+    return fn_global; \
+  if constexpr (std::same_as<T, VarGlobal>) \
+    return var_global; \
+  if constexpr (std::same_as<T, TypeGlobal>) \
+    return type_global; \
+  if constexpr (std::same_as<T, AliasGlobal>) \
+    return alias_global;
+
+  /// @brief Represents a global.
+  /// Rather than using an inheritance, we make use of a variant.
   struct GlobalVariant
   {
+    template<typename T, typename... Args>
+    /// @brief Helper to construct the union
+    /// @param placement The pointer to the object to construct
+    /// @param args... The arguments to forward to the constructor
+    /// @return Empty monostate
+    static constexpr std::monostate construct(T* placement, Args&&... args)
+    {
+      // Use construct_at rather than placement new for constexpr
+      std::construct_at(placement, std::forward<Args>(args)...);
+      return {};
+    }
 
+    /// @brief The possible types contained in the variant
+    union
+    {
+      // Helper used in constructor
+      std::monostate   _mono_state_;
+      /// @brief The function global
+      FnGlobal         fn_global;
+      /// @brief The variable global
+      VarGlobal        var_global;
+      /// @brief The type global
+      TypeGlobal       type_global;
+      /// @brief The alias global
+      AliasGlobal      alias_global;
+    };
+
+    template<typename T>
+    /// @brief Returns a reference to the union member of type 'T'
+    /// @return Reference to the union member of type 'T'
+    constexpr const auto& getUnionMember() const noexcept { COLTC_getUnionMember; }
+
+    template<typename T>
+    /// @brief Returns a reference to the union member of type 'T'
+    /// @return Reference to the union member of type 'T'
+    constexpr auto& getUnionMember() noexcept { COLTC_getUnionMember; }
+
+  public:
+    template<ColtGlobal Type, typename... Args>
+    /// @brief Constructor
+    /// @param args... The arguments to forward to the constructor
+    constexpr GlobalVariant(std::type_identity<Type>, Args&&... args)
+      noexcept(std::is_nothrow_constructible_v<Type, Args...>)
+      : _mono_state_(construct<Type>(&getUnionMember<Type>(), std::forward<Args>(args)...))
+    {
+      // The ugly code above is used to keep the constructor constexpr
+    }
+
+    constexpr GlobalVariant(GlobalVariant&&) noexcept = default;
+    constexpr GlobalVariant(const GlobalVariant&) noexcept = default;
+    constexpr GlobalVariant& operator=(GlobalVariant&&) noexcept = default;
+    constexpr GlobalVariant& operator=(const GlobalVariant&) noexcept = default;
+
+    /// @brief Returns the global ID of the current type
+    /// @return The ID of the current type
+    constexpr GlobalID getGlobalID() const noexcept
+    {
+      // We cannot read the value through any of the members
+      // because of UB.
+      // The memcpy is optimized away by the compiler.
+      GlobalID id;
+      if (std::is_constant_evaluated()) // bit_cast is constexpr...
+        id = std::bit_cast<GlobalID>(fn_global);
+      else
+        std::memcpy(&id, &fn_global, sizeof(GlobalID));
+      return id;
+    }
+
+    template<ColtGlobal T>
+    /// @brief Casts the current type to 'T'
+    /// @tparam T The type to cast to
+    /// @return nullptr if the variant does not contain 'T', or valid pointer to 'T'
+    constexpr T* getGlobal() noexcept
+    {
+      if (getGlobalID() != TypeToGlobalID<T>())
+        return nullptr;
+      return &getUnionMember<T>();
+    }
+    
+    template<ColtGlobal T>
+    /// @brief Casts the current type to 'T'
+    /// @tparam T The type to cast to
+    /// @return nullptr if the variant does not contain 'T', or valid pointer to 'T'
+    constexpr const T* getGlobal() const noexcept
+    {
+      if (getGlobalID() != TypeToGlobalID<T>())
+        return nullptr;
+      return &getUnionMember<T>();
+    }
+
+    /// @brief Check if the Global is a function
+    /// @return True if function
+    constexpr bool isFn() const noexcept { return getGlobalID() == GlobalID::GLOBAL_FN; }
+    /// @brief Check if the Global is a global variable
+    /// @return True if variable
+    constexpr bool isVar() const noexcept { return getGlobalID() == GlobalID::GLOBAL_VAR; }
+    /// @brief Check if the Global is a type
+    /// @return True if type
+    constexpr bool isType() const noexcept { return getGlobalID() == GlobalID::GLOBAL_TYPE; }
+    /// @brief Check if the Global is an alias
+    /// @return True if an alias
+    constexpr bool isAlias() const noexcept { return getGlobalID() == GlobalID::GLOBAL_ALIAS; }
   };
 }
+
+#undef COLTC_getUnionMember
 
 #endif // !HG_COLT_GLOBAL
