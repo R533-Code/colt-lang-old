ca8156b2147e12aa629464c83ad409103d831d52
R533-Code <raphaeldibnehmepc@gmail.com>
2024-03-16 17:58:23 +0100

    Added new files with barebone structure

---
+++ src/ast/ast.cpp
@@ -0,0 +1,9 @@
+#include "ast.h"
+
+namespace clt::lng
+{
+  void MakeAST(ParsedUnit& unit) noexcept
+  {
+
+  }
+}
---
+++ src/ast/ast.h
@@ -0,0 +1,17 @@
+#ifndef HG_COLT_AST
+#define HG_COLT_AST
+
+#include "parsed_unit.h"
+#include "colt_expr.h"
+
+namespace clt::lng
+{
+  void MakeAST(ParsedUnit& unit) noexcept;
+
+  struct ASTMaker
+  {
+
+  };
+}
+
+#endif // !HG_COLT_AST
