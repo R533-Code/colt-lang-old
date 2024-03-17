480ee51100c3846adf130ffa339f792c754e2590
R533-Code <raphaeldibnehmepc@gmail.com>
2024-03-17 21:23:42 +0100

    Added implementation of some expressions

--- src/ast/colt_expr.h
+++ src/ast/colt_expr.h
@@ -8,11 +8,12 @@
 #ifndef HG_COLT_EXPR
 #define HG_COLT_EXPR
 
+#include "lng/macro_helper.h"
 #include "lex/colt_token_buffer.h"
 #include "lng/colt_type_buffer.h"
 
 DECLARE_ENUM_WITH_TYPE(u8, clt::lng, ExprID,
-  EXPR_ERROR, EXPR_LITERAL, EXPR_UNARY, EXPR_BINARY,
+  EXPR_ERROR, EXPR_LITERAL, EXPR_UNARY, EXPR_BINARY, EXPR_CAST,
   // VARIABLE RELATED
   EXPR_ADDRESSOF,
   EXPR_VAR_DECL, EXPR_VAR_READ, EXPR_VAR_WRITE,
@@ -21,18 +22,18 @@ DECLARE_ENUM_WITH_TYPE(u8, clt::lng, ExprID,
   // PTR RELATED
   EXPR_PTR_LOAD, EXPR_PTR_STORE,
   // FN RELATED
-  EXPR_DECL_FN,
+  EXPR_DECL_FN, EXPR_CALL_FN,
   // OTHER
   EXPR_SCOPE, EXPR_CONDITION
 );
 
 /// @brief Macro Expr List (with same index as ExprID declaration!)
-#define COLTC_EXPR_LIST ErrorExpr, LiteralExpr, UnaryExpr, BinaryExpr, \
+#define COLTC_EXPR_LIST ErrorExpr, LiteralExpr, UnaryExpr, BinaryExpr, CastExpr, \
    AddressOfExpr, VarDeclExpr, VarReadExpr, VarWriteExpr, \
    GlobalDeclExpr, GlobalReadExpr, GlobalWriteExpr, \
    MoveExpr, CopyExpr, CMoveExpr, \
    PtrLoadExpr, PtrStoreExpr, \
-   FnDeclExpr, \
+   FnDeclExpr, FnCallExpr, \
    ScopeExpr, ConditionExpr
 
 namespace clt::lng
@@ -40,9 +41,17 @@ namespace clt::lng
   // Forward declarations
   FORWARD_DECLARE_TYPE_LIST(COLTC_EXPR_LIST);
   // TypeToExprID
-  CONVERT_TYPE_TO_ENUM(ExprID, COLTC_EXPR_LIST);
+  CONVERT_TYPES_TO_ENUM(ExprID, COLTC_EXPR_LIST);
 
-  using ExprToken = u32;
+  /// @brief Represents any expression that produces a value.
+  /// Can be any of [ErrorExpr, LiteralExpr, UnaryExpr, BinaryExpr, CastExpr,
+  /// AddressOfExpr, PtrLoadExpr, VarReadExpr, GlobalReadExpr, FnCallExpr]
+  using ProdExprToken = u32;
+  /// @brief Represents any of [ErrorExpr, VarDeclExpr, VarWriteExpr,
+  /// GlobalDeclExpr, PtrStoreExpr, GlobalWriteExpr, MoveExpr, CopyExpr, CMoveExpr]
+  using  VarExprToken = u32;
+  /// @brief Represents any of [ErrorExpr, ScopeExpr, ConditionExpr]
+  using StmtExprToken = u32;
 
   /// @brief Base class of all expressions
   class ExprBase
@@ -53,19 +62,28 @@ namespace clt::lng
     TokenRange range;
     /// @brief The ID of the expression
     ExprID expr_id;
+  
+  protected:
+    /// @brief Byte that can be used for anything
+    u8 padding0 = 0;
+    /// @brief Byte that can be used for anything
+    u8 padding1 = 0;
+    /// @brief Byte that can be used for anything
+    u8 padding2 = 0;
 
   public:
     /// @brief Constructor
     /// @param id The ID of the expression
     /// @param type The type of the expression
     /// @param range The range representing the expression
-    constexpr ExprBase(ExprID id, TypeToken type, TokenRange range) noexcept
-      : type(type), range(range), expr_id(id) {}
+    /// @param padding0 Byte that can be used by the child class
+    /// @param padding1 Byte that can be used by the child class
+    /// @param padding2 Byte that can be used by the child class
+    constexpr ExprBase(ExprID id, TypeToken type, TokenRange range, u8 padding0 = 0, u8 padding1 = 0, u8 padding2 = 0) noexcept
+      : type(type), range(range), expr_id(id), padding0(padding0), padding1(padding1), padding2(padding2) {}
 
-    constexpr ExprBase(ExprBase&&) noexcept = default;
-    constexpr ExprBase(const ExprBase&) noexcept = default;
-    constexpr ExprBase& operator=(ExprBase&&) noexcept = default;
-    constexpr ExprBase& operator=(const ExprBase&) noexcept = default;
+    ExprBase() = delete;
+    MAKE_DEFAULT_COPY_AND_MOVE_FOR(ExprBase);
 
     /// @brief Returns the range of tokens representing the expression
     /// @return Range of tokens reprsenting the expression
@@ -95,15 +113,129 @@ namespace clt::lng
     constexpr ErrorExpr(TokenRange range, TypeToken type) noexcept
       : ExprBase(TypeToExprID<ErrorExpr>(), type, range) {}
 
-    constexpr ErrorExpr(ErrorExpr&&) noexcept = default;
-    constexpr ErrorExpr(const ErrorExpr&) noexcept = default;
-    constexpr ErrorExpr& operator=(ErrorExpr&&) noexcept = default;
-    constexpr ErrorExpr& operator=(const ErrorExpr&) noexcept = default;
+    MAKE_DEFAULT_COPY_AND_MOVE_FOR(ErrorExpr);
+  };
+
+  /// @brief Represents a literal expression
+  class LiteralExpr
+    final : public ExprBase
+  {
+    /// @brief The value of the literal
+    QWORD_t value;
+
+  public:
+    /// @brief Constructor
+    /// @param range The range of tokens
+    /// @param type The type of the literal
+    /// @param value The literal value
+    constexpr LiteralExpr(TokenRange range, TypeToken type, QWORD_t value) noexcept
+      : ExprBase(TypeToExprID<LiteralExpr>(), type, range), value(value) {}
+
+    MAKE_DEFAULT_COPY_AND_MOVE_FOR(LiteralExpr);
+
+    /// @brief Return the value of the literal
+    /// @return The value of the literal
+    constexpr QWORD_t getValue() const noexcept { return value; }
+  };
+
+  /// @brief Represents a unary expression
+  class UnaryExpr
+    final : public ExprBase
+  {
+    /// @brief The expression on which the operation is applied
+    ProdExprToken expr;
+
+    // The operator is stored in the padding bits of ExprBase.
+
+  public:
+    /// @brief Constructor
+    /// @param range The range of tokens
+    /// @param type The type of the expression (same as 'expr')
+    /// @param op The operation
+    /// @param expr The expression on which the unary operation is applied
+    constexpr UnaryExpr(TokenRange range, TypeToken type, UnaryOp op, ProdExprToken expr) noexcept
+      : ExprBase(TypeToExprID<UnaryExpr>(), type, range, static_cast<u8>(op)), expr(expr) {}
+
+    MAKE_DEFAULT_COPY_AND_MOVE_FOR(UnaryExpr);
+
+    /// @brief Returns the expression on which the unary operation is applied.
+    /// @return The expression on which the unary operation is applied
+    constexpr ProdExprToken getExpr() const noexcept { return expr; }
+    
+    /// @brief Returns the operation applied on the expression
+    /// @return Any unary op
+    constexpr UnaryOp getOp() const noexcept { return static_cast<UnaryOp>(ExprBase::padding0); }
+  };
+
+  /// @brief Reprents a binary operation
+  class BinaryExpr
+    final : public ExprBase
+  {
+    /// @brief The left hand side of the expression
+    ProdExprToken lhs;
+    /// @brief The right hand side of the expression
+    ProdExprToken rhs;
+
+    // The operator is stored in the padding bits of ExprBase.
+
+  public:
+    /// @brief Constructor
+    /// @param range The range of tokens
+    /// @param type The type of the expression (can differ if comparison)
+    /// @param lhs The left hand side of the expression
+    /// @param op The operation applied on the expression
+    /// @param rhs The right hand side of the expression
+    constexpr BinaryExpr(TokenRange range, TypeToken type, ProdExprToken lhs, BinaryOp op, ProdExprToken rhs) noexcept
+      : ExprBase(TypeToExprID<UnaryExpr>(), type, range, static_cast<u8>(op)), lhs(lhs), rhs(rhs) {}
+
+    MAKE_DEFAULT_COPY_AND_MOVE_FOR(BinaryExpr);
+
+    /// @brief Returns the left hand side of the expression
+    /// @return The left hand side
+    constexpr ProdExprToken getLHS() const noexcept { return lhs; }
+    /// @brief Returns the right hand side of the expression
+    /// @return The right hand side
+    constexpr ProdExprToken getRHS() const noexcept { return rhs; }
+
+    /// @brief Returns the operation applied on the expression
+    /// @return Any binary op that is not an assignment operator
+    constexpr BinaryOp getOp() const noexcept { return static_cast<BinaryOp>(ExprBase::padding0); }
+  };
+
+  /// @brief Cast from built-in type to built-in type
+  class CastExpr
+    final : public ExprBase
+  {
+    /// @brief The expression to cast
+    ProdExprToken to_cast;
+
+  public:
+    /// @brief Constuctor
+    /// @param range The range of tokens
+    /// @param type The type to cast to
+    /// @param to_cast The expression to cast
+    constexpr CastExpr(TokenRange range, TypeToken cast_to, ProdExprToken to_cast, bool is_bit_cast) noexcept
+      : ExprBase(TypeToExprID<CastExpr>(), cast_to, range, is_bit_cast), to_cast(to_cast) {}
+
+    MAKE_DEFAULT_COPY_AND_MOVE_FOR(CastExpr);
+
+    /// @brief Returns the expression to cast
+    /// @return The expression to cast
+    constexpr ProdExprToken getToCast() const noexcept { return to_cast; }
+
+    /// @brief Returns the type to cast
+    /// @return The type to cast to
+    constexpr TypeToken getTypeToCastTo() const noexcept { return getType(); }
+
+    /// @brief Check if the cast is a bit cast
+    /// @return True if bit cast
+    constexpr bool isBitCast() const noexcept { return ExprBase::padding0; }
   };
 
   class ExprBuffer
   {
-
+  
+  public:
   };
 }
 
