e9f022468e32064e6232d25dbf701b2e77c0a011
R533-Code <raphaeldibnehmepc@gmail.com>
2024-06-25 02:28:41 +0200

    Added some helper methods for ColtiExecutable

---
+++ src/backend/colti/colti_exe.cpp
@@ -0,0 +1,124 @@
+#include "colti_exe.h"
+
+namespace clt::run
+{
+  Option<ColtiExecutable> ColtiExecutable::load(View<u8> bytes) noexcept
+  {
+    assert_true(
+        "Bytes must be aligned!",
+        (uintptr_t)bytes.data() % alignof(ColtiHeader) == 0);
+
+    if (bytes.size() < sizeof(ColtiHeader))
+      return None;
+    // We now need to check that this is a valid header
+    if (reinterpret_cast<const ColtiHeader*>(bytes.data())->magic_number
+        != ColtiHeader::MAGIC_NUMBER)
+      return None;
+    return ColtiExecutable(bytes);
+  }
+
+  Option<std::chrono::year> ColtiExecutable::year() const noexcept
+  {
+    const i32 value = (u8)header()->compilation_date_year;
+    if (value == 0)
+      return None;
+    return std::chrono::year(value + 2024);
+  }
+
+  Option<std::chrono::month> ColtiExecutable::month() const noexcept
+  {
+    const u8 value = (u8)header()->compilation_date_month;
+    if (value == 0 || value > 12)
+      return None;
+    return std::chrono::month(value);
+  }
+
+  Option<std::chrono::day> ColtiExecutable::day() const noexcept
+  {
+    const u8 value = (u8)header()->compilation_date_day;
+    if (value == 0)
+      return None;
+    return std::chrono::day(value);
+  }
+
+  Option<std::chrono::minutes> ColtiExecutable::minute() const noexcept
+  {
+    // compilation_date_min == 1 -> hour == 0
+    // compilation_date_min == 60 -> hour == 59
+    u32 min = (u32)header()->compilation_date_min;
+    if (min == 0 || min > 60)
+      return None;
+    return std::chrono::minutes{min};
+  }
+
+  Option<std::chrono::hours> ColtiExecutable::hour() const noexcept
+  {
+    u32 hour = (u32)header()->compilation_date_hour;
+    // compilation_date_hour == 1 -> hour == 0
+    // compilation_date_hour == 12 -> hour == 11
+    if (hour == 0 || hour > 12)
+      return None;
+    hour--;
+    hour += 12 * (u8)(!header()->compilation_date_am);
+    return std::chrono::hours{hour};
+  }
+
+  ColtVersion ColtiExecutable::version() const noexcept
+  {
+    return {
+        (u8)header()->colt_version_major, (u8)header()->colt_version_minor,
+        (u8)header()->colt_version_patch};
+  }
+
+  u16 ColtiExecutable::section_count() const noexcept
+  {
+    return header()->section_count;
+  }
+
+  ExecutableSection ColtiExecutable::section(u16 index) const noexcept
+  {
+    assert_true("Invalid index!", index < section_count());
+
+    const u8* base_offset = bytes.data();
+    auto section_name =
+        reinterpret_cast<const char*>(base_offset + section_offsets()[index]);
+
+    // The size of the 'section_name' string (which can be
+    // at maximum 31) without the NUL-terminator
+    u64 section_name_size = 0;
+    for (size_t i = 0; i < 32; i++)
+    {
+      if (section_name[i] == '\0')
+        break;
+      section_name_size++;
+    }
+    // Align pointer to the next u64
+    auto section_size_ptr = reinterpret_cast<const u64*>(
+        align_to_next<8>(section_name + section_name_size + 1));
+
+    return ExecutableSection{
+        StringView{section_name, section_name_size},
+        reinterpret_cast<const u8*>(section_size_ptr + 1), *section_size_ptr};
+  }
+
+  StringView ColtiExecutable::section_name(u16 index) const noexcept
+  {
+    return section(index).name;
+  }
+
+  Option<ExecutableSection> ColtiExecutable::find_section(
+      StringView name) const noexcept
+  {
+    for (size_t i = 0; i < section_count(); i++)
+      if (auto section_ = section(i); section_.name == name)
+        return section_;
+    return None;
+  }
+
+  View<u64> ColtiExecutable::section_offsets() const noexcept
+  {
+    return {
+        reinterpret_cast<const u64*>(bytes.data() + sizeof(ColtiHeader)),
+        section_count()};
+  }
+} // namespace clt::run
--- src/backend/colti/colti_exe.h
+++ src/backend/colti/colti_exe.h
@@ -5,17 +5,6 @@
 
 namespace clt::run
 {
-  /// @brief Represents a section of a Colti executable
-  struct ColtiSection
-  {
-    /// @brief The name of the section
-    StringView name;
-    /// @brief The beginning of the section
-    const u8* begin;
-    /// @brief The end of the section
-    const u8* end;
-  };
-
   /// @brief Represents the starting header of a Colti executable.
   /// All numbers are in little endian.
   struct ColtiHeader
@@ -26,46 +15,136 @@ namespace clt::run
     /// @brief The number of section in the current executable
     u16 section_count;
 
-    // GROUP OF 2 BYTES vvv
-
     /// @brief The major version of the language
     u16 colt_version_major : 5;
     /// @brief The minor version of the language
     u16 colt_version_minor : 5;
-    /// @brief The tweak version of the language
-    u16 colt_version_tweak : 6;
+    /// @brief The patch version of the language
+    u16 colt_version_patch : 6;
 
-    // GROUP OF 2 BYTES vvv
-
-    /// @brief True if the 'compilation_date_hour' (below) is AM, false if PM
-    u16 compilation_date_am : 1;
-    /// @brief The compilation day (0 means no date information, 1 -> Monday...)
-    u16 compilation_date_day : 3;
-    /// @brief The compilation month (0 means no date information, 1 -> January...)
-    u16 compilation_date_month : 4;
-    /// @brief The compilation year (0 means no date information, 1 -> 2024...)
-    u16 compilation_date_year : 8;
-
-    // GROUP OF 2 BYTES vvv
-
-    /// @brief The compilation second (0 means no date information, 1 -> 0s...)
-    u16 compilation_date_sec : 6;
-    /// @brief The compilation minute (0 means no date information, 1 -> 0m...)
-    u16 compilation_date_min : 6;
     /// @brief The compilation hour (0 means no date information, 1 -> 0h).
     /// If 'compilation_date_am' is false then we add 12 to the encoded hour.
     u16 compilation_date_hour : 4;
-    
+    /// @brief The compilation month (0 means no date information, 1 -> January...)
+    u16 compilation_date_month : 4;
+    /// @brief True if the 'compilation_date_hour' is AM, false if PM
+    u16 compilation_date_am : 1;
+    /// @brief The compilation minute (0 means no date information, 1 -> 0m...)
+    u16 compilation_date_min : 6;
+    /// @brief The compilation day (0 means no date information, [1-31]...)
+    u16 compilation_date_day : 5;
+    /// @brief The compilation year (0 means no date information, 1 -> 2024...)
+    u16 compilation_date_year : 11;
+
     /// @brief This must be equal to 'MAGIC_NUMBER'
     u32 magic_number;
+    /// @brief Unused for now
+    u32 padding;
+    
+    // After the padding, there is a u64 containing the offset
+    // to each section.
+    // Each section begins with the NUL-terminated section name
+    // (31 characters at most, without counting NUL-terminator),
+    // followed by a u64 representing the size in bytes of the
+    // content, then followed by the content of the section.
+    // The content of the section must always be 8-byte aligned.
+    // u64 offset_to_section[section_count]
+  };
+  
+  template<u8 ALIGN, typename T>
+    requires std::is_pointer_v<T> || meta::UnsignedIntegral<T>
+  constexpr T align_to_next(T to_align) noexcept;
+
+  /// @brief Represents an executable section
+  struct ExecutableSection
+  {
+    /// @brief The name of the section
+    StringView name;
+    /// @brief The beginning of the section
+    const u8* begin;
+    /// @brief The size of the section
+    u64 size;
   };
 
   class ColtiExecutable
   {
-    //ColtiExecutable()
-  public:
+    View<u8> bytes;
 
+    ColtiExecutable(View<u8> bytes) noexcept
+        : bytes(bytes)
+    {
+    }
+  
+  public:
+    static Option<ColtiExecutable> load(View<u8> bytes) noexcept;   
+
+    /// @brief Returns the ColtiHeader of the executable.
+    /// It is error prone to use the header directly: every information
+    /// it provides can be accessed safely using the other member functions.
+    /// @return The ColtiHeader of the executable
+    const ColtiHeader* header() const noexcept
+    {
+      return reinterpret_cast<const ColtiHeader*>(bytes.data());
+    }
+
+    /// @brief Returns the compilation year of the executable
+    /// @return Year or None if the info is unavailable
+    Option<std::chrono::year> year() const noexcept;
+    /// @brief Returns the compilation month of the executable
+    /// @return Month that is always ok() or None if the info is unavailable
+    Option<std::chrono::month> month() const noexcept;
+    /// @brief Returns the compilation year of the executable
+    /// @return Year or None if the info is unavailable
+    Option<std::chrono::day> day() const noexcept;
+    /// @brief Returns the compilation minute of the executable.
+    /// If a value is return, it will be in range [0, 59].
+    /// @return Minute or None if the info is unavailable
+    Option<std::chrono::minutes> minute() const noexcept;
+    /// @brief Returns the compilation hour of the executable.
+    /// If a value is return, it will be in range [0, 23].
+    /// @return Hour or None if the info is unavailable
+    Option<std::chrono::hours> hour() const noexcept;
+
+    /// @brief Returns the language version of the compiled executable
+    /// @return The language version of the compiled executable
+    ColtVersion version() const noexcept;
+
+    /// @brief Returns the number of sections contained in the executable
+    /// @return The section count
+    u16 section_count() const noexcept;
+
+    /// @brief Returns the section at index 'index'
+    /// @param index The index of the section (index < section_count)
+    /// @return Section at index 'index'
+    ExecutableSection section(u16 index) const noexcept;
+
+    /// @brief Returns the section name at index 'index'
+    /// @param index The index of the section (index < section_count)
+    /// @return The section name
+    StringView section_name(u16 index) const noexcept;
+
+    /// @brief Searches for a section of name 'name'
+    /// @param name The name of the section
+    /// @return The section or None if not found
+    Option<ExecutableSection> find_section(StringView name) const noexcept;
+
+    /// @brief Returns a view over the sections offset
+    /// @return View over the sections offset
+    View<u64> section_offsets() const noexcept;
+
+    /// @brief Check if an offset points inside the executable
+    /// @param offset The offset (from the start of the executable)
+    /// @return True if the offset points inside the executable
+    bool is_in_range(u64 offset) const noexcept { return offset < bytes.size(); }
   };
+
+  template<u8 ALIGN, typename T>
+    requires std::is_pointer_v<T> || meta::UnsignedIntegral<T>
+  constexpr T align_to_next(T to_align) noexcept
+  {
+    const auto VALUE = reinterpret_cast<uintptr_t>(to_align) % ALIGN;
+    return to_align + (ALIGN - VALUE) * (VALUE != 0);
+  }
 
 } // namespace clt::run
 
